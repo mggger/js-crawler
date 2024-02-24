1f87ea29def8729b697a60d896569fccba00107a
mggger <mggis0or1@gmail.com>
2024-02-24 14:19:46 +0800

    update

--- index.js
+++ index.js
@@ -1,34 +1,6 @@
 import puppeteer from 'puppeteer';
 import fs from 'fs';
 
-// Increase the navigation timeout for Puppeteer
-const navigationOptions = {
-    waitUntil: 'networkidle2',
-    timeout: 60000 // Setting timeout to 60 seconds
-};
-
-// Function to process specific URLs to extract titles and subtitles
-const processUrls = async (browser, urls) => {
-    const results = [];
-    for (let url of urls) {
-        const page = await browser.newPage();
-        await page.goto(url, navigationOptions);
-        const {title, subtitle} = await page.evaluate(() => {
-            const h1Text = document.querySelector('h1') ? document.querySelector('h1').innerText : null;
-            const h2Text = document.querySelector('h2') ? document.querySelector('h2').innerText : h1Text; // Use h1 value if h2 does not exist
-            return {title: h1Text, subtitle: h2Text};
-        });
-        results.push({url, title, subtitle});
-        await page.close();
-    }
-    return results;
-};
-
-// Function to read URLs from a file
-const readUrlsFromFile = (filePath) => {
-    const data = fs.readFileSync(filePath, 'utf8');
-    return data.split('\n').filter(line => line.trim() !== '').map(line => line.trim());
-};
 
 // Read records from legislation_menu.csv
 const readMenu = async (filePath) => {
@@ -61,8 +33,7 @@ const scrapeData = async (browser, url) => {
 // Main function to merge data and write to a file
 const mergeAndWriteData = async () => {
     const browser = await puppeteer.launch({headless: false});
-    const urls = readUrlsFromFile("just_content.csv"); // Reading URLs from file
-    const specificUrlsResults = await processUrls(browser, urls); // Processing specific URLs
+
     const records = await readMenu('legislation_menu.csv'); // Reading records from file
 
     const newDataset = [];
@@ -73,15 +44,14 @@ const mergeAndWriteData = async () => {
         for (let toc of tocData) {
             if (toc.text !== 'Title') {
                 const newUrl = 'https://www.legislation.govt.nz' + toc.href;
-                newDataset.push({title, subTitle: toc.text, url: newUrl});
+                const combinedTitle = `${toc.text}(${title})`; // Combine title and toc text with a dash
+                newDataset.push({description: combinedTitle, url: newUrl});
             }
         }
     }
 
     await browser.close();
-    const finalDataset = [...specificUrlsResults, ...newDataset]; // Combining datasets
-
-    fs.writeFileSync('new_zealand_legislation_list.json', JSON.stringify(finalDataset, null, 2)); // Writing data to file
+    fs.writeFileSync('new_zealand_legislation_list.json', JSON.stringify(newDataset, null, 2)); // Writing data to file
 };
 
 mergeAndWriteData().catch(console.error);
